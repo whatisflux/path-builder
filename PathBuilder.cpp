2d11325594b0e2e81e80c0defc5ce1f597c0ca71
craigmc08 <craigmc08@gmail.com>
2019-05-16 10:26:22 -0400

    Make float serializer use unsigned data types

--- PathBuilder.cpp
+++ PathBuilder.cpp
@@ -3,14 +3,14 @@
 std::string pb::serializeFloat(float v)
 {
     // Convert pointer to the float to a pointer to an int, and then deref it to get a value
-    int bits = *(int*)&v;
+    unsigned bits = *(unsigned*)&v;
     std::string serialized;
     for (int byte = 0; byte < 4; byte++)
     {
         // Shift the wanted bits to the lowest 8 bits
         // Bitwise-and it with 255 to clear out unwanted higher bits
-        int val = (bits >> (8 * (3 - byte))) & 0b11111111;
-        serialized += (char)val;
+        unsigned val = (bits >> (8 * (3 - byte))) & 0b11111111;
+        serialized += (unsigned char)val;
     }
     return serialized;
 }
